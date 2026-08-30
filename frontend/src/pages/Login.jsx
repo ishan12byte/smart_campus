3ec836a455f2b3072ff05efddb07a032755fc97f
Ishika Vig <ishikavig18@gmail.com>
2026-08-30 14:28:21 +0530

    Create React frontend foundation

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+function Login() {
+  return (
+    <div>
+      <h1>SMART CAMPUS</h1>
+
+      <label>Email</label>
+      <input type="email" />
+
+      <label>Password</label>
+      <input type="password" />
+
+      <button>Login</button>
+      
+      <p>
+        Forgot Password?
+      </p>
+    </div>
+  )
+}
+export default Login
---
+++ frontend/src/pages/StaffDashboard.jsx
@@ -0,0 +1,6 @@
+function StaffDashboard(){
+    return(
+        <h1>Staff Dashboard</h1>
+    )
+}
+export default StaffDashboard
