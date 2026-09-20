d040186540cd6007f1b91a3c592f8e2cd43c61c4
Ishika Vig <ishikavig18@gmail.com>
2026-09-20 15:02:05 +0530

    feat(frontend): connect login to auth services

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,21 +1,45 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import {login} from '../services/auth'
+
 function Login() {
+
   const navigate = useNavigate()
+  const [email, setEmail] = useState("")
+  const [password, setPassword]= useState("")
+
+  const handleLogin= async()=>{
+    try{
+      const data=await login(email,password)
+      console.log(data)
+    }
+    catch(error){
+
+    }
+  }
   return (
     <div>
       <h1>SMART CAMPUS</h1>
 
       <label>Email</label>
-      <input type="email" />
+      <input 
+      type="email" 
+      value={email}
+      onChange={(event)=> setEmail(event.target.value)}
+      />
 
       <br/>
 
       <label>Password</label>
-      <input type="password" />
+      <input 
+      type="password" 
+      value={password}
+      onChange={(event)=> setPassword(event.target.value)}
+      />
 
       <br/>
 
-      <button onClick={()=>navigate("/student")}>Login</button>
+      <button onClick={handleLogin}>Login</button>
       
       <p>
         Forgot Password?
--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,7 +1,15 @@
-import {apiRequest} from "./api";
-export async function login(email,password){
-    const data=await apiRequest("/auth/login,{
-        method:"POST",
-        
-    )
+import { apiRequest } from "./api";
+
+export async function login(email, password) {
+    const data = await apiRequest("/auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    });
+    return data;
 }
